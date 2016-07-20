a7af9112ac4d5ecf7b8def28cbeeeaa5a53ae315
Julien - Parsonii <j.carette@parsonii.com>
2016-07-20 12:17:08 +0200

    Initial
    Initial

---
+++ plugins/wordpress/bulle-instagram/bulle-instagram.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: Bulle Instagram
+Plugin URI: http://bulle.parsonii.com/plugins/wordpress/bulle-instagram
+Description: Intégration des desniers posts de votre compte Instagram directement sur votre site WordPress
+Version: 1.0
+Author: Parsonii
+Author URI: http://www.parsonii.com/
+License: GPLv2
+*/
+
+define( 'SBIVER', '1.0' );
+
+// Add shortcodes
+add_shortcode('bulle-instagram', 'bulle_instagram');
+function bulle_instagram($atts, $content = null) {
+	$r = "";
+
+	$r .= "test<br/>";
+
+	return $r;
+}
